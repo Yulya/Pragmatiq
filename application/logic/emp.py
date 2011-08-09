67af4ed94cbce168b1c72c0e7383bc38159867c6
Yulya <julja_portnova@mail.ru>
2011-08-09 15:55:47 +0300

    deleted emp.py

--- application/logic/emp.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import db
-
-class Employee(db.Model):
-    first_name = db.StringProperty()
-    last_name = db.StringProperty()
-    e_mail = db.StringProperty()
-    salary = db.IntegerProperty()
-    first_date = db.DateProperty()
