5318c7b3cfb8d398abc21e0d4aabf2262c6720fd
Yulya <julja_portnova@mail.ru>
2011-11-09 11:07:47 +0200

    PRAG-104

--- application/logic/web_handlers/get_eaf_handler.py
+++ application/logic/web_handlers/get_eaf_handler.py
@@ -5,12 +5,19 @@ from logic.web_handlers.base_form_handler import BaseFormHandler
 class GetEmployeeForm(BaseFormHandler):
 
     template_values = {}
-    path = 'templates/api.employee_form.html'
+    path = 'templates/api.readonly_eaf.html'
     type = 'employee'
 
     def get(self, key):
 
         super(GetEmployeeForm, self).get(key)
+
+        user = self.template_values['user']
+        employee = self.template_values['form'].pr.employee
+        status = self.template_values['form'].status
+
+        if user.email == employee.email and status == 'draft':
+            self.path = 'templates/api.employee_form.html'
 
         self.response.out.write(template.render(self.path,
                                                 self.template_values))
