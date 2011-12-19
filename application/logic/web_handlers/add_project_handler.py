69860d90e54d50d4739f5f3887825e18ad941256
Yulya <julja_portnova@mail.ru>
2011-12-19 15:25:20 +0200

    150

--- application/logic/web_handlers/add_project_handler.py
+++ application/logic/web_handlers/add_project_handler.py
@@ -1,5 +1,5 @@
 from google.appengine.ext.webapp import RequestHandler
-from logic.models import WorkProject
+from logic.models import WorkProject, User
 
 
 class AddProjects(RequestHandler):
@@ -7,9 +7,12 @@ class AddProjects(RequestHandler):
     def post(self):
 
         name = self.request.get('name')
-        
+        manager = self.request.get('manager')
+        manager = User.all().filter('email', manager).get()
         project = WorkProject(name=name,
+                              manager=manager,
                           employees = [])
         project.put()
 
-        self.response.out.write('ok')
+        self.response.out.write('You have successfully created project "%s"'
+                                % project.name)
--- application/logic/web_handlers/attach_employee_to_project.py
+++ application/logic/web_handlers/attach_employee_to_project.py
@@ -9,7 +9,6 @@ class AttachEmployeeToProject(RequestHandler):
     def post(self):
 
         project_key = self.request.get('project_key')
-        logging.debug(project_key)
         users = self.request.get('employees')
         try:
             project = WorkProject.get(project_key)
@@ -18,12 +17,25 @@ class AttachEmployeeToProject(RequestHandler):
             return
 
         user_emails = users.split(',')
+        employees = map(lambda x: User.get(x).email, project.employees)
+        attached_users = []
+        existed_users = []
 
         for email in user_emails:
             if email:
                 user = User.all().filter('email', email).get()
                 if user:
-                    project.employees.append(user.key())
-                    project.put()
+                    logging.debug(user)
+                    if user.email not in employees:
+                        project.employees.append(user.key())
+                        project.put()
+                        attached_users.append(user.email)
+                    else:
+                        existed_users.append(user.email)
+        if attached_users:
+            attached_users = (', ').join(attached_users)
+            self.response.out.write('You have attached to "%s" %s' % (project.name, attached_users))
+        if existed_users:
+            existed_users = (', ').join(existed_users)
+            self.response.out.write('%s are already attached to "%s' % (existed_users, project.name))
 
-        self.response.out.write('ok')
