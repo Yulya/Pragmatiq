90d785f991aba68d12ee001612a76411af2e9da3
unknown <Yulya@JP-HOME.(none)>
2011-12-26 13:59:01 +0200

    PRAG-12

--- application/logic/web_handlers/automatic_creation_pr_handler.py
+++ application/logic/web_handlers/automatic_creation_pr_handler.py
@@ -8,53 +8,88 @@ class AutomaticPerformanceReview(RequestHandler):
 
     def get(self):
         today = datetime.date.today()
-        events = Event.all().filter('start_date', today)
-
-        nearest_event = None
-        #TODO: nearest_event_date
+        events = Event.all()
         employees = User.all()
-        for event in events:
-            description = "PR %s: %s-%s" % (event.type,
-                                            event.start_date,
-                                            event.finish_date)
+        month = datetime.timedelta(days=30)
+        week = datetime.timedelta(weeks=1)
 
-            period = PerformanceReviewPeriod(type=event.type,
-                                             description=description,
-                                             start_date=event.start_date,
-                                             finish_date=event.finish_date)
-            period.put()
-            for employee in employees:
+        for employee in employees:
+            if employee.first_date:
+                if employee.first_date + 2 * month == today:
 
-                last_pr = PerformanceReview.all().filter(
-                                    'employee', employee).order('-date').get()
+                    first_date = employee.first_date
+                    start_date = first_date + datetime.timedelta(weeks=13)
+                    finish_date = start_date + datetime.timedelta(weeks=2)
+                    description = "%s %s first review" %(employee.first_name, employee.last_name)
 
-                if last_pr:
-                    last_date = last_pr.date
-                    month = datetime.timedelta(days=30)
-                    review_next_date = last_date + 6 * month
-                    delta = review_next_date - nearest_event.start_date
-                    if delta <= month and delta >= -month:
-                        if nearest_event.start_date == today:
+                    period = PerformanceReviewPeriod(type='annual',
+                                                    description=description,
+                                                    start_date=start_date,
+                                                    finish_date=finish_date)
+                    period.put()
 
-                            pr = PerformanceReview(employee=employee,
-                                                   manager=employee.manager,
-                                                   first_effective_date=event.
-                                                   first_effective_date,
-                                                   date=period.start_date,
-                                                   period=period)
-                            pr.put()
-                    else:
-                        if review_next_date == today:
-                            period = PerformanceReviewPeriod(type=nearest_event.type,
-                                             description=description,
-                                             start_date=event.start_date,
-                                             finish_date=event.finish_date)
-                            period.put()
-                            pr = PerformanceReview(employee=employee,
-                                                   manager=employee.manager,
-                                                   first_effective_date=event.
-                                                   first_effective_date,
-                                                   date=period.start_date,
-                                                   period=period)
-                            pr.put()
+                    pr = PerformanceReview(employee=employee,
+                                            manager=employee.manager,
+                                            date=period.start_date,
+                                            period=period)
+                    pr.put()
+
+            elif len(employee.self_pr.fetch(100)) > 0:
+                last_pr = employee.self_pr.order('-date').get()
+                last_pr_date = last_pr.period.start_date
+                next_pr_start_date = last_pr_date + 6 * month
+                next_pr_start_date = next_pr_start_date.replace(year=today.year)
+
+                events = Event.all()
+
+                next_date_near_event = 0
+                create_event_today = None
+
+                for event in events:
+                    if event.start_date < today:
+                        event.start_date = event.start_date.replace(year = (today.year + 1))
+                        event.put()
+                    delta = next_pr_start_date - event.start_date
+                    if event.start_date - month == today or event.start_date.replace(year=(today.year + 1)):
+                        create_event_today = event
+
+                    if delta < month and delta > -month:
+                        next_date_near_event = 1
+
+                if not next_date_near_event:
+
+                    if next_pr_start_date < today:
+                        next_pr_start_date.replace(year=(today.year + 1))
+                    if next_pr_start_date == today + month:
+                        period = PerformanceReviewPeriod(start_date=next_pr_start_date,
+                            description='self period',
+                            finish_date=next_pr_start_date + 2 * week)
+                        period.put()
+
+                        pr = PerformanceReview(period=period,
+                            employee=employee,
+                            manager=employee.manager,
+                            date=period.start_date
+                        )
+                        pr.put()
+
+                elif create_event_today:
+
+                    delta = next_pr_start_date - create_event_today.start_date
+
+                    if delta < month and delta > -month:
+
+                        description = 'aaa'
+                        period = PerformanceReviewPeriod(start_date=event.start_date,
+                            description=description,
+                            finish_date=event.finish_date)
+                        period.put()
+
+                        pr = PerformanceReview(period=period,
+                            employee=employee,
+                            manager=employee.manager,
+                            date=period.start_date
+                        )
+                        pr.put()
+
 
