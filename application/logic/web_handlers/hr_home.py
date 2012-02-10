da48f4bc454ef1bacf3d6635a58777250bbd5f17
Yulya <julja_portnova@mail.ru>
2012-02-10 20:54:36 +0200

    ----kkk

--- application/logic/web_handlers/hr_home.py
+++ application/logic/web_handlers/hr_home.py
@@ -15,26 +15,26 @@ class HRHome(RequestHandler):
             period.delete = 'inline'
             period.departments = []
 
-            for pr in period.performance_reviews:
+            period.prs = period.performance_reviews.fetch(1000)
+            for pr in period.prs:
                 if pr.period.is_open:
                     if not pr.employee.dept.key() in period.departments:
                         period.departments.append(pr.employee.dept.key())
             period.departments = map(lambda x: Dept.get(x), period.departments)
 
-            for pr in period.performance_reviews:
+            for pr in period.prs:
                 if pr.manager_form or pr.employee_form:
                     period.delete = 'none'
                 pr.register = 'disabled'
-                if pr.manager_form:
-                    if pr.manager_form.status == 'submitted':
-                        period.register = ''
-                        pr.register = ''
 
             for department in period.departments:
-                department.prs = filter(lambda x: x.employee.dept.name == department.name, period.performance_reviews)
-
-
-#                period.pr.append(pr)
+                department.register = 'disabled'
+                department.prs = filter(lambda x: x.employee.dept.name == department.name, period.prs)
+                for pr in department.prs:
+                    if pr.manager_form:
+                        if pr.manager_form.status == 'submitted':
+                            department.register = ''
+                            pr.register = ''
 
         template_values = {'open_periods': open_periods,
                            'closed_periods': closed_periods}
